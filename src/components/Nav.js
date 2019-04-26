3fc0fdeb235776a6093b079a135ee2468e452ff2
James Hwang <james.hwang@cyndx.net>
2019-04-26 10:30:07 -0400

    Initial commit

---
+++ src/components/Nav.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Nav = () => {
+  return(
+    <nav>
+      <ul>
+        <li>Menu 1</li>
+        <li>Menu 2</li>
+        <li>Menu 3</li>
+        <li>Menu 4</li>
+      </ul>
+    </nav>
+  )
+}
+
+export default Nav
